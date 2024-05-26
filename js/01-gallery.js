2a5c9450296ba41659775d5b9df6397ff2992246
Evgeniy Yurchenko <o6uga.ga@gmail.com>
2024-05-26 13:10:24 +0200

    Update 01-gallery.js

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -1,13 +1,25 @@
 import { galleryItems } from './gallery-items.js';
 // Change code below this line
 
+const instance = basicLightbox.create(`
+	<h1>Dynamic Content</h1>
+	<p>You can set the content of the lightbox with JS.</p>
+`);
+instance.show(() => console.log('lightbox now visible'));
+
 const galleryImg = document.querySelector('.gallery');
 
-const createListItemsMarkup = galleryItems
+const itemsMarkup = galleryItems
   .map(
     item =>
       `<li class="gallery__item"><img class="gallery__image" src="${item.preview}" alt="${item.description}"></li>`
   )
   .join('');
 
-galleryImg.insertAdjacentHTML('beforeend', createListItemsMarkup);
+galleryImg.insertAdjacentHTML('beforeend', itemsMarkup);
+
+galleryImg.addEventListener('click', onClickgallery);
+
+function onClickgallery(evt) {
+  console.log(evt);
+}
