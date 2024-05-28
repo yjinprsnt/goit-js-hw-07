789423fb96522da31b9e1f29e9232b903f633031
Evgeniy Yurchenko <o6uga.ga@gmail.com>
2024-05-28 20:17:21 +0200

    Update 02-lightbox.js

--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -1,4 +1,25 @@
 import { galleryItems } from './gallery-items.js';
 // Change code below this line
 
-console.log(galleryItems);
+// console.log(galleryItems);
+
+const galleryImg = document.querySelector('.gallery');
+const itemsMarkup = galleryItems
+  .map(
+    ({ preview, original, description }) =>
+      `<li class="gallery__item">
+    <a class="gallery__link" href="${original}">
+      <img class="gallery__image" src="${preview}" 
+      alt="${description}" />
+    </a>
+  </li>`
+  )
+  .join('');
+
+galleryImg.insertAdjacentHTML('beforeend', itemsMarkup);
+
+const lightbox = new SimpleLightbox('.gallery__link', {
+  captionsData: 'alt',
+  captionPosition: 'bottom',
+  captionDelay: 250,
+});
